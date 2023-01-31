2f817bfdbd495d933529ba49c2b15593c8befd0a
Pratik Panda <ppanda.soccerrules@gmail.com>
2023-01-30 22:28:39 -0700

    Updated some stylings to look more uniform

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ linkRequest.onreadystatechange = function () {
         if (linkRequest.status === 200 || linkRequest.status == 0) {
             var html = "";
             JSON.parse(linkRequest.responseText).forEach(function (link) {
-                console.log(link.name);
                 html += "<div class=\"links waves-effect\"><a href=" + link.url + " class=\"black-text valign-wrapper\" target=\"_blank\"><i class=\"link_icon mdi mdi-" + link.icon + "\" style=\"color: " + link.color + "\"></i>&nbsp; " + link.name + "</a></div>";
             });
             link.innerHTML = html;
@@ -29,8 +28,28 @@ workRequest.onreadystatechange = function () {
         if (workRequest.status === 200 || workRequest.status == 0) {
             var html = "";
             JSON.parse(workRequest.responseText).forEach(function (work) {
-                console.log(work.name);
-                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span><p><a href=" + work.link + " target=\"_blank\"><i class=\"link_icon mdi mdi-code-tags\" style=\"color: black; font-size: 24px\"></i></a></p></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work.name + "<i class=\"material-icons right\">close</i></span><p>" + work.description + "</p></div></div></div>";
+                html +=
+                    "<div class=\"work-card\">"
+                    +   "<div class=\"card hoverable\">"
+                    +       "<div class=\"card-image waves-effect waves-block waves-light\">"
+                    +           "<img class=\"activator work-image\" src=" + work.image + ">"
+                    +       "</div>"
+                    +       "<div class=\"card-content\">"
+                    +           "<span class=\"card-title activator grey-text text-darken-4\">"
+                    +               work.name
+                    +               "<i class=\"material-icons right\">keyboard_arrow_up</i>"
+                    +           "</span>"
+                    +           "<p><a href=" + work.link + " target=\"_blank\"><i class=\"link_icon mdi mdi-code-tags\" style=\"color: black; font-size: 24px\"></i></a></p>"
+                    +       "</div>"
+                    +       "<div class=\"card-reveal\">"
+                    +           "<span class=\"card-title grey-text text-darken-4\">"
+                    +               work.name
+                    +               "<i class=\"material-icons right\">close</i>"
+                    +           "</span>"
+                    +           "<p>" + work.description + "</p>"
+                    +       "</div>"
+                    +   "</div>"
+                    + "</div>";
             });
             work.innerHTML = html;
         }
